e36999eea9e68cc96d261e403a1d740e8a615bab
--get <--get>
2026-05-12 15:08:46 +0300

    All files and folders created, Register and Login started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Suspense } from 'react';
+import { useRoutes } from 'react-router-dom';
+import publicRoutes from './api/router/routes/publicRoutes';
+
+export default function App() {
+  console.log(publicRoutes)
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {useRoutes(publicRoutes)}
+    </Suspense>
+  );
+}
