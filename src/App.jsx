667a884ee0b2c4232b49b20399d76c70f82b1ac8
--get <--get>
2026-05-12 15:53:31 +0300

    Login and Register forms info completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useRoutes } from 'react-router-dom';
 import publicRoutes from './api/router/routes/publicRoutes';
 
 export default function App() {
-  console.log(publicRoutes)
   return (
     <Suspense fallback={<div>Loading...</div>}>
       {useRoutes(publicRoutes)}
